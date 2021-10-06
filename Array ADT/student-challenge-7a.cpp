efc865074ae1de645506835b6077eadf853f66a4
Ananyaa Bansal <itsananyaa@gmail.com>
2021-10-06 22:23:28 +0530

    hacktober3

--- Array ADT/student-challenge-7a.cpp
+++ Array ADT/student-challenge-7a.cpp
@@ -1,34 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 //program to find pair of elements with a particular sum in unsorted array O(n sq.)
-int main(){
+int main()
+{
 
     int n;
-    int sum, count=0;
-    cout<<"Enter size of array: ";
-    cin>>n;
+    int sum, count = 0;
+
+    cout << "Enter Size of array: ";
+    cin >> n;
     int A[n];
-    cout<<"Enter elements in array without duplication : ";
-    for(int i=0; i<n; i++)
-        cin>>A[i];
+    cout << "Enter elements in array without duplication: ";
 
-    cout<<"\nEnter the sum value you want to find additive pair for: ";
-    cin>>sum;
-    cout<<endl;
+    for (int i = 0; i < n; i++)
+        cin >> A[i];
 
-    for(int i=0; i<n-1; i++)
+    cout << "\nEnter the sum value you want to find additive pair for: ";
+    cin >> sum;
+    cout << endl;
+
+    for (int i = 0; i < n - 1; i++)
     {
-        for(int j=i+1; j<n; j++)
+        for (int j = i + 1; j < n; j++)
         {
-            if(A[i]+A[j]==sum)
+            if (A[i] + A[j] == sum)
             {
-                cout<<"The pair is: ("<<A[i]<<","<<A[j]<<")\n";
+                cout << "The pair is: (" << A[i] << "," << A[j] << ")\n";
+
                 count++;
             }
         }
     }
-    cout<<"Total number of pairs: "<<count;
+    cout << "Total number of pairs: " << count;
 }
-
-
